bde0b9c89ac0fe505ff47f816dad2d991c976383
Nolan Meyer <102100700+Lil-Junko@users.noreply.github.com>
2023-10-13 01:22:04 -0700

    Update lexer.c
    It was a mistake to think I should become a computer science major

--- lexer.c
+++ lexer.c
@@ -1,7 +1,11 @@
 #include "lexer.h"
 
+/*
+* brief: these consts don't work in lexer.h, the compiler thinks there are two, here is fine
+* I just want this to be done, please
+*/
 //list of operators
-const char operators[] = {'<', '>', '(', ')', '+', '-', '*', '/', '|', '&', ';', ',', ':', '[', ']', '=', '!'};
+const char operators[] = {'<', '>', '(', ')', '+', '-', '*', '/', '|', '&', ';', ',', ':', '[', ']', '=', '!'}; //yes '.' is missing
 const char* doubleOperators [] = {":=", "..", "<<", ">>", "<>", "<=", ">=", "**", "!=","=>" };
 // accessor and array begin bool case character constant else elsif end exit function
 // if in integer interface is loop module mutator natural null of or other out
@@ -10,14 +14,17 @@ const char* keywords[] = {"accessor", "and", "array", "begin", "bool", "case", "
                     "if", "in", "integer", "interface", "is", "loop", "module", "mutator", "natural", "null", "of", "or", "other", "out", "positive",
                     "procedure", "range", "return", "struct", "subtype", "then", "type", "when", "while"};
 
+/*
+*@brief: this function checks to see if there is a double operator in the word
+*/
 bool containsDoubleOperator(char word[])
 {
-    //int size = strlen(word);
-    int operatorSize = sizeof(doubleOperators)/sizeof(doubleOperators[0]);
+    int size = strlen(word);
+    int operatorSize = sizeof(doubleOperators)/sizeof(doubleOperators[0]); //total number of operators
     
-    for(int i = 0; i < operatorSize; i++)
+    for(int i = 0; i < operatorSize; i++) //check through all operators
     {
-        if(strstr(word, doubleOperators[i]))
+        if(strstr(word, doubleOperators[i])) //also check through all operators
         {
             return true;
         }
@@ -26,6 +33,9 @@ bool containsDoubleOperator(char word[])
     return false;
 }
 
+/*
+*@brief: this function parses the word and determines what it is it is a mess and I am dissapointed in my ability to code
+*/
 void parseWord(char word[])
 {
     if (isNumber(word)) //Numbers
@@ -55,11 +65,15 @@ void parseWord(char word[])
         fprintf(fptr2, "%s (UNK)\n", word);
     }
 }
-
+/*
+* @brief: this function checks to see if the word is an operator
+* @note: you may notice I am not check for '.' here, that is because I am tired and it breaks everything and reading through
+*        everything has become a nightmare, I don't think I can fix it without breaking everything else
+*/
 bool isOperator(char word[])
 {
     //total number of characters in word[]
-    //int size = strlen(word);
+    int size = strlen(word);
     int operatorSize = sizeof(operators)/sizeof(operators[0]);
     
     for(int j = 0; j < operatorSize; j++)
@@ -75,34 +89,42 @@ bool isOperator(char word[])
 
     return false;
 }
-
+/*
+* @brief: this function checks to see if the word is a double operator
+* @note: this looks similar to containsDoubleOperator, but unlike that function, this one checks to see if the word is a double operator
+*        this is accatable to me
+*/
 bool isDoubleOperator(char word[])
 {
     //total number of characters in word[]
-    //int size = strlen(word);
-    int operatorSize = sizeof(doubleOperators)/sizeof(doubleOperators[0]);
+    int size = strlen(word); //total number of characters in word[]
+    int operatorSize = sizeof(doubleOperators)/sizeof(doubleOperators[0]); //total number of operators
     
     for(int i = 0; i < operatorSize; i++)
     {
-        if(strstr(word, doubleOperators[i]))
+        if(strstr(word, doubleOperators[i])) //check through all operators
         {
-            return true;
+            return true; //if it is a double operator, return true
         }
     }
 
     return false;
 }
 
+/*
+* @brief: this function gets the index of the operator in the word
+* @note: this is used to determine how much of the word is an operator and where to end it
+*/
 int getOperatorIndex(char word[])
 {
     //total number of characters in word[]
     int size = strlen(word);
-    size_t sizeOperators = sizeof(operators)/sizeof(operators[0]);;
-    size_t sizeDoubleOperators = sizeof(doubleOperators)/sizeof(doubleOperators[0]);
+    size_t sizeOperators = sizeof(operators)/sizeof(operators[0]); //total number of operators
+    size_t sizeDoubleOperators = sizeof(doubleOperators)/sizeof(doubleOperators[0]); //total number of double operators
 
     for(int j = 0; j < size; j++)
     {
-        for(int i = 0; i < sizeOperators; i++)
+        for(int i = 0; i < sizeOperators; i++) //check through all operators
         {
             char temp[2];
             temp[0] = word[j];
@@ -125,6 +147,10 @@ int getOperatorIndex(char word[])
     return 0;
 }
 
+/*
+* @brief: self explanitory, checks to see if the word is a number
+* there are some cases when operators fight with isNumber
+*/
 bool isNumber(char word[])
 {
     int size = strlen(word);
@@ -135,54 +161,59 @@ bool isNumber(char word[])
 
     for(int i = 0; i < size; i++)
     {
-        if(strstr(word, ".."))
+        if(strstr(word, "..")) //this is a special case, it is a double operator, so we can have our decimal
         {
             return false;
         }
         
-        bool a = isdigit(word[i]);
-        bool b = (word[i] == '.');
-        bool c = (word[i] == '-');
+        bool a = isdigit(word[i]); //is it a digit
+        // bool b = (word[i] == '.'); //is it a decimal
+        bool c = (word[i] == '-'); //is it a negative sign
 
-        if(!c)
+        if(!c) //if it is not a negative sign
         {
-            if(!b)
-            {
-                if(!a)
+            // if(!b) //if it is not a decimal            
+            // {
+                if(!a) //if it is not a digit
                 {
-                    return false;
+                    return false; //it is not a number
                 }
-            }
+            // }
         }
     }
-    
     return true;
 }
 
-//why did I make this so simple? Simple really, I don't how to specifically check for identifiers
-//the only real rule for it is that it starts with a letter, can be anything else besides that as long as there is no space in it
-//so I'm just going to check if it starts with a letter and if it does, then it's an identifier
-bool isIdentifier(char word[])
-{
-    //does it start with a letter upper or lowercase
-    if(word[0] < 'A' || word[0] > 'z') //do you have a better idea for checks?
-    {
-        return false;
-    }
-
-    return true;
-}
-
+/*
+* @brief: this function checks to see if the word is a keyword
+* Hey I'm actually proud of this because it works as designed and I didn't have to change anything
+* also these were out of order before I moved them
+*/
 bool isKeyword(char word[])
 {
-    size_t size = sizeof(keywords)/sizeof(keywords[0]);;
+    size_t size = sizeof(keywords)/sizeof(keywords[0]); //total number of keywords
 
     for(int i = 0; i < size; i++)
     {
-        if(strcmp(word, keywords[i]) == 0)
+        if(strcmp(word, keywords[i]) == 0) //check through all keywords
         {
             return true;
         }
     }
     return false;
+}
+
+/*why did I make this so simple? Simple really, I don't how to specifically check for identifiers
+*the only real rule for it is that it starts with a letter, can be anything else besides that as long as there is no space in it
+*so I'm just going to check if it starts with a letter and if it does, then it's an identifier
+*/
+bool isIdentifier(char word[])
+{
+    //does it start with a letter upper or lowercase
+    if(word[0] < 'A' || word[0] > 'z') //do you have a better idea for checks?
+    {
+        return false; //I don't
+    }
+
+    return true;
 }
